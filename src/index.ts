c2a7802233ca8bab0562b428a98270233051a8ae
Azeezbank <bankoleazeezb98@gmail.com>
2025-08-11 21:20:02 +0100

    updated

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 // app.use(passport.initialize());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Alabamarket API');
