5d526d024c52130dcafe08880e9bf70c8249e741
Azeezbank <bankoleazeezb98@gmail.com>
2025-09-11 11:22:41 +0100

    updated endpoint lock

--- src/routes/Buyer.ts
+++ src/routes/Buyer.ts
@@ -16,29 +16,29 @@ const router = express.Router();
 
 
 router.put('/account', authenticate, upload.single('file'), buyerSetting);
-router.get('/listing/categories', authenticate, fetchcategories);
+router.get('/listing/categories', fetchcategories);
 // router.get('/active/seller/listing', authenticate, storeFetchActiveSellerListings);
-router.get('/popular/listing', authenticate, filterpopularListings);
-router.get('/listing/price/range', authenticate, filterListingsByPriceRange);
-router.get('/listing/price/less', authenticate, filterListingsByLessPrice);
-router.get('/listing/price/greater', authenticate, filterListingsByGreaterPrice);
+router.get('/popular/listing', filterpopularListings);
+router.get('/listing/price/range', filterListingsByPriceRange);
+router.get('/listing/price/less', filterListingsByLessPrice);
+router.get('/listing/price/greater', filterListingsByGreaterPrice);
 // router.get('/all/listing', authenticate, FetchAllSellerListings);
 router.get('/verified/seller/listing', authenticate, fetchVerifiedSellerListing);
 router.get('/unverified/seller/listing', authenticate, fetchUnverifiedSellerListing);
-router.get('/listing/condition', authenticate, fetchSellerListingByCondition);
-router.get('/listing/owner/details/:productId', authenticate, productOwner);
+router.get('/listing/condition', fetchSellerListingByCondition);
+router.get('/listing/owner/details/:productId', productOwner);
 router.get('/saved/product', authenticate, getSavedProduct);
-router.get('/all/active/listing', authenticate, getActiveListing);
+router.get('/all/active/listing', getActiveListing);
 router.get('/all/notification', authenticate, getBuyerallNotifications);
 router.get('/all/read/notification', authenticate, getBuyerReadNotifications);
 router.get('/all/unread/notification', authenticate, getBuyerUnReadNotifications);
 router.post('/question', authenticate, handleQuestion);
-router.post('/like/unlike/:productId', authenticate, likes);
+router.post('/like/unlike/:productId', likes);
 router.post('/saved/listing/:productId', authenticate, savedProduct);
-router.post('/love/:productId', authenticate, love);
+router.post('/love/:productId', love);
 router.post('/report/listing/:productId', authenticate, productReport);
-router.get('/average/rating/:productId', authenticate, productReviewRatingAvg);
-router.get('/rating/distribution/:productId', authenticate, productratingDistribution);
+router.get('/average/rating/:productId', productReviewRatingAvg);
+router.get('/rating/distribution/:productId', productratingDistribution);
 router.put('/rating/:productId', authenticate, productrating);
 
 
